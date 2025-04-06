389a3f4b043846b6ab4e290cddb1a24b8f953741
Rocksri <srinathanand1979@gmail.com>
2025-04-06 14:47:40 +0530

    bg design done

--- src/profile/homepage.jsx
+++ src/profile/homepage.jsx
@@ -1,16 +1,56 @@
-
 export default function HomePage() {
     return (
-        <div className="grid grid-cols-8 grid-rows-6 gap-7 Home_Page">
-            <div className="grid_design col-span-4 col-start-3">1</div>
-            <div className="grid_design col-span-2 row-span-3 row-start-3">2</div>
-            <div className="grid_design col-span-2 row-span-3 col-start-7 row-start-3">
-                3
-            </div>
-            <div className="grid_design col-span-2 row-span-4 col-start-4 row-start-3">
-                4
-            </div>
-        </div>
-    );
 
+        <div className="grid grid-cols-5 grid-rows-3 gap-7 Home_Page">
+            <div className="grid_design tri1">1</div>
+            <div className="grid_design tri2">2</div>
+            <div className="grid_design tri3">3</div>
+            <div className="grid_design tri4">4</div>
+            <div className="grid_design tri5 col-start-2 row-start-2">5</div>
+            <div className="grid_design tri6 col-start-3 row-start-2">6</div>
+            <div className="grid_design tri7 col-start-4 row-start-2">7</div>
+            <div className="grid_design tri8 col-start-2 row-start-3">8</div>
+            <div className="grid_design tri9 col-start-3 row-start-3">9</div>
+            <div className="grid_design tri10 col-start-4 row-start-3">10</div>
+            <div className="grid_design tri11 col-start-5 row-start-3">11</div>
+        </div>
+
+        // <div className="flex flex-wrap gap-4 justify-start items-start Home_Page">
+        //     <div className="grid_design tri1 flex-none w-12 h-12 bg-gray-200 text-center">
+        //         1
+        //     </div>
+        //     <div className="grid_design tri2 flex-none w-12 h-12 bg-gray-300 text-center">
+        //         2
+        //     </div>
+        //     <div className="grid_design tri3 flex-none w-12 h-12 bg-gray-400 text-center">
+        //         3
+        //     </div>
+        //     <div className="grid_design tri4 flex-none w-12 h-12 bg-gray-500 text-center">
+        //         4
+        //     </div>
+
+        //     <div className="grid_design tri5 flex-none w-12 h-12 bg-gray-600 text-center">
+        //         5
+        //     </div>
+        //     <div className="grid_design tri6 flex-none w-12 h-12 bg-gray-700 text-center">
+        //         6
+        //     </div>
+        //     <div className="grid_design tri7 flex-none w-12 h-12 bg-gray-800 text-center">
+        //         7
+        //     </div>
+
+        //     <div className="grid_design tri8 flex-none w-12 h-12 bg-gray-900 text-center">
+        //         8
+        //     </div>
+        //     <div className="grid_design tri9 flex-none w-12 h-12 bg-gray-100 text-center">
+        //         9
+        //     </div>
+        //     <div className="grid_design tri10 flex-none w-12 h-12 bg-gray-200 text-center">
+        //         10
+        //     </div>
+        //     <div className="grid_design tri11 flex-none w-12 h-12 bg-gray-300 text-center">
+        //         11
+        //     </div>
+        // </div>
+    );
 }
